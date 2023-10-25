c40f00db83d30d5514a6514783be56d93b516428
Rachael Robinson <rachael.robinson@cabinetoffice.gov.uk>
2023-10-25 13:06:55 +0100

    use models.TextChoices

--- evaluation_registry/evaluations/models.py
+++ evaluation_registry/evaluations/models.py
@@ -20,7 +20,16 @@ class TimeStampedModel(models.Model):
         abstract = True
 
 
-class ChoicesModel(models.Model):
+class User(BaseUser, UUIDPrimaryKeyBase):
+    objects = BaseUserManager()
+    username = None
+
+    def save(self, *args, **kwargs):
+        self.email = self.email.lower()
+        super().save(*args, **kwargs)
+
+
+class Department(UUIDPrimaryKeyBase, TimeStampedModel):
     code = models.SlugField(
         max_length=128,
         unique=True,
@@ -39,35 +48,28 @@ class ChoicesModel(models.Model):
         abstract = True
 
 
-class User(BaseUser, UUIDPrimaryKeyBase):
-    objects = BaseUserManager()
-    username = None
-
-    def save(self, *args, **kwargs):
-        self.email = self.email.lower()
-        super().save(*args, **kwargs)
-
-
-class Department(UUIDPrimaryKeyBase, TimeStampedModel, ChoicesModel):
-    pass
-
-
-class EvaluationType(UUIDPrimaryKeyBase, TimeStampedModel, ChoicesModel):
-    pass
-
-
-class EvaluationVisibility(UUIDPrimaryKeyBase, TimeStampedModel, ChoicesModel):
-    pass
-
-
 class Evaluation(UUIDPrimaryKeyBase, TimeStampedModel):
-    created_by = models.ForeignKey(User, on_delete=models.CASCADE)
+    class EvaluationType(models.TextChoices):
+        IMPACT = 'impact', 'Impact evaluation'
+        PROCESS = 'process', 'Process evaluation'
+        ECONOMIC = 'economic', 'Economic evaluation'
+        OTHER = 'other', 'Other'
+
+    class EvaluationVisibility(models.TextChoices):
+        DRAFT = 'draft', 'Draft'
+        CIVIL_SERVICE = 'civil_service', 'Civil Service'
+        PUBLIC = 'public', 'Public'
+
+    created_by = models.ForeignKey(User, on_delete=models.CASCADE, blank=True, null=True)
 
     title = models.CharField(max_length=1024, blank=True, null=True)
-    lead_department = models.ForeignKey(Department, on_delete=models.CASCADE)
+    lead_department = models.ForeignKey(Department, on_delete=models.CASCADE, related_name='led_evaluations')
     departments = models.ManyToManyField(Department, related_name="+")
 
-    evaluation_type = models.ManyToManyField(EvaluationType)
+    evaluation_type = models.CharField(
+        max_length=25,
+        choices=EvaluationType.choices,
+    )
     evaluation_type_other = models.CharField(max_length=256, blank=True, null=True)
 
     # TODO: confirm whether this should be a CharField with design team
@@ -83,7 +85,11 @@ class Evaluation(UUIDPrimaryKeyBase, TimeStampedModel):
     # TODO: there is a standard max_length of 200 for URL Fields - will that be enough?
     plan_link = models.URLField(blank=True, null=True)
     published_evaluation_link = models.URLField(blank=True, null=True)
-    visibility = models.ForeignKey(EvaluationVisibility, on_delete=models.CASCADE)
+    visibility = models.CharField(
+        max_length=25,
+        choices=EvaluationVisibility.choices,
+        default=EvaluationVisibility.DRAFT
+    )
 
 
 class EventDate(UUIDPrimaryKeyBase, TimeStampedModel):
