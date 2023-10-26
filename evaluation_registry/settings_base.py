989db3e711510f50c699e8ba05ee1dca6388939e
george <g.e.c.burton@gmal.com>
2023-10-26 09:36:05 +0100

    fix build steps

--- evaluation_registry/settings_base.py
+++ evaluation_registry/settings_base.py
@@ -38,6 +38,31 @@ MIDDLEWARE = [
     "allauth.account.middleware.AccountMiddleware",
 ]
 
+TEMPLATES = [
+    {
+        "BACKEND": "django.template.backends.jinja2.Jinja2",
+        "DIRS": [
+            BASE_DIR / "evaluation_registry" / "templates",
+        ],
+        "OPTIONS": {"environment": "evaluation_registry.jinja2.environment"},
+    },
+    {
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": [
+            BASE_DIR / "evaluation_registry" / "templates" / "allauth",
+        ],
+        "APP_DIRS": True,
+        "OPTIONS": {
+            "context_processors": [
+                "django.template.context_processors.debug",
+                "django.template.context_processors.request",
+                "django.contrib.auth.context_processors.auth",
+                "django.contrib.messages.context_processors.messages",
+            ],
+        },
+    },
+]
+
 STATIC_URL = "/static/"
 STATICFILES_DIRS = [BASE_DIR / "static"]
 STATIC_ROOT = BASE_DIR / "staticfiles"
