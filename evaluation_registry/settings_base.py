afee658f0ca1b6a60592ca24e1fa05ae61dfb748
george <g.e.c.burton@gmal.com>
2023-10-26 09:31:18 +0100

    fix build steps

--- evaluation_registry/settings_base.py
+++ evaluation_registry/settings_base.py
@@ -10,11 +10,16 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = env.str("DJANGO_SECRET_KEY")
 
 INSTALLED_APPS = [
+    "evaluation_registry.evaluations",
     "allauth",
     "allauth.account",
     "allauth.socialaccount",
     "django.contrib.admin",
+    "django.contrib.auth",
     "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.messages",
+    "django.contrib.sites",
     "django.contrib.staticfiles",
 ]
 
