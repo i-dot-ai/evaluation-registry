3de2381f2ae890f0a429178f0f1d44d800d58fad
Elliot Moore <elliotmoorecornwall@outlook.com>
2023-11-03 13:42:27 +0000

    Added auth urls

--- evaluation_registry/urls.py
+++ evaluation_registry/urls.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.urls import include, path
 
-from evaluation_registry.evaluations import info_views, views
+from evaluation_registry.evaluations import info_views, views, authentication_views, decorators
 
 info_urlpatterns = [
     path("privacy-notice/", info_views.privacy_notice_view, name="privacy-notice"),
@@ -9,12 +9,18 @@ info_urlpatterns = [
 ]
 
 other_urlpatterns = [
-    path("", views.evaluation_list_view, name="index"),
+    path("", authentication_views.CustomLoginView.as_view(), name="index"),
+    path("unauthorised/", decorators.unauthorised_view, name="unauthorised"),
+    path("evaluations/", views.evaluation_list_view, name="list-evaluations"),
     path("health/", include("health_check.urls")),
     path("home/", views.homepage_view, name="homepage"),
     path("admin/", admin.site.urls),
     path("evaluation/<uuid:uuid>/", views.evaluation_detail_view, name="evaluation-detail"),
-    path("accounts/", include("allauth.urls")),
+    path("logout/", authentication_views.LogoutView.as_view(), name="logout"),
+    path("email-sent/", authentication_views.email_sent_view, name="email-sent"),
+    path("post-login/", authentication_views.post_login_view, name="post-login"),
+    path("verify-register/", authentication_views.register_email_view, name="verify-email-register"),
+    path("verify/", authentication_views.verify_email_view, name="verify-email"),
 ]
 
 urlpatterns = info_urlpatterns + other_urlpatterns
