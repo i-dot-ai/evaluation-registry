b13baaba17285e065fa0eb0f7d1688faea7d69b4
george <g.e.c.burton@gmal.com>
2023-11-23 11:04:05 +0000

    now opening file and converting to text

--- evaluation_registry/evaluations/admin.py
+++ evaluation_registry/evaluations/admin.py
@@ -28,7 +28,8 @@ def import_csv(modeladmin, request, queryset):
         return
 
     file = queryset.first()
-    records = list(csv.DictReader(file.csv.file))
+    text_file = (line.decode() for line in file.csv.file)
+    records = list(csv.DictReader(text_file))
     cmd = load_rsm_csv.Command()
     cmd.process_tabular_data(records)
     file.last_successfully_loaded_at = datetime.datetime.now()
