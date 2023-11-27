10ce88b7bc2f16355f4f99196ec98a54501e6f67
George Burton <8233643+gecBurton@users.noreply.github.com>
2023-11-27 15:03:25 +0000

    Update settings_base.py (#51)

--- evaluation_registry/settings_base.py
+++ evaluation_registry/settings_base.py
@@ -37,6 +37,8 @@ ALLOWED_HOSTS = [
     "127.0.0.1",
     "evaluation-registry-dev.eba-izdb4qxe.eu-west-2.elasticbeanstalk.com",
     "evaluation-registry-prod.eba-izdb4qxe.eu-west-2.elasticbeanstalk.com",
+    "dev.evaluation-registry.service.gov.uk",
+    "evaluation-registry.service.gov.uk",
 ]
 
 # CSRF settings
