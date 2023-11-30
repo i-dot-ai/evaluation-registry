b6ddbad6171194aabbf5ab10daf7fe3d52af73b6
george <g.e.c.burton@gmal.com>
2023-11-30 18:00:00 +0000

    added EVAREG_COGNITO_FIXED_PASSWORD envvar

--- evaluation_registry/evaluations/management/commands/create_users.py
+++ evaluation_registry/evaluations/management/commands/create_users.py
@@ -1,25 +1,37 @@
 import boto3
 from botocore.exceptions import ClientError
 from django.conf import settings
-from django.core.exceptions import ImproperlyConfigured
 from django.core.management import BaseCommand
 from tqdm import tqdm
-
-from evaluation_registry import settings
-from evaluation_registry.evaluations.models import Evaluation
 
 cognito_client = boto3.client("cognito-idp", region_name=settings.AWS_REGION_NAME)
 
 
-def create_user(email: str) -> None:
-    name = email.split("@")[0]
-    given_name, family_name = name.split(".", 1)
+def user_exists(email: str) -> bool:
+    try:
+        cognito_client.admin_get_user(
+            UserPoolId=settings.COLA_COGNITO_USER_POOL_ID,
+            Username=email,
+        )
+        return True
+    except ClientError:
+        return False
 
+
+def create_user(
+    email: str,
+    given_name: str,
+    family_name: str,
+    is_admin: bool | None = False,
+    phone_number: str | None = None,
+) -> None:
     user_attributes = [
         {"Name": "given_name", "Value": given_name},
         {"Name": "family_name", "Value": family_name},
         {"Name": "email", "Value": email},
         {"Name": "email_verified", "Value": "true"},
+        {"Name": "custom:isAdmin", "Value": is_admin},
+        {"Name": "custom:phoneNumber", "Value": phone_number},
     ]
 
     cognito_client.admin_create_user(
@@ -47,16 +59,10 @@ class Command(BaseCommand):
         progress_bar = tqdm(desc="Processing", total=len(options["emails"]))
         for email in options["emails"]:
             progress_bar.set_description(f"creating user: {email}")
-            try:
-                cognito_client.admin_get_user(
-                    UserPoolId=settings.COLA_COGNITO_USER_POOL_ID,
-                    Username=email,
-                )
-                self.stdout.write(self.style.SUCCESS("user already exists"))
-            except ClientError:
-                create_user(email)
-                self.stdout.write(self.style.SUCCESS("created user"))
-
+            if not user_exists(email):
+                name = email.split("@")[0]
+                given_name, family_name = name.split(".", 1)
+                create_user(email, given_name, family_name)
             progress_bar.update(1)
 
         progress_bar.close()
