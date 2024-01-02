7a7bcec6106949cda7a46abeb0dfcb10b75bcd7a
George Burton <8233643+gecBurton@users.noreply.github.com>
2024-01-02 15:46:56 +0000

    Update docker/web/start.sh

--- docker/web/start.sh
+++ docker/web/start.sh
@@ -3,6 +3,6 @@
 set -o errexit
 set -o nounset
 
-poetry run python manage.py migrate
+poetry run python manage.py migrate --noinput
 poetry run python manage.py collectstatic --noinput
 poetry run python manage.py runserver 0.0.0.0:8000
